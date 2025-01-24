031237b6c75c1262a9a77d3f2eb3408ed96ae22f
soi <zest1923@gmail.com>
2025-01-24 23:37:20 +0900

    Initial Commit

---
+++ NavigationStackTests/NavigationStackTests.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationStackTests.swift
+//  NavigationStackTests
+//
+//  Created by soi on 1/24/25.
+//
+
+import Testing
+
+struct NavigationStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
